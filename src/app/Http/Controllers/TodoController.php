faadbe7870ce965bf56119dc5a1da75c53c346c7
kameyama <eri.kameyama@giztech-pro.com>
2024-06-20 13:04:44 +0900

    add: DBのデータを取得する

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -1,14 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Todo;
 
 class TodoController extends Controller
 {
 	public function index()
 	{
+		$todo = new Todo();
+		$todoList = $todo->all();
+		dd($todoList);
+
 		return view('todo.index');
 	}
-
 }
