4df44fcb647dc0a5d1d5203b12b55810c6f51595
kameyama <eri.kameyama@giztech-pro.com>
2024-06-20 10:58:49 +0900

    add: todosにレコード追加

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -5,12 +5,14 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * un the databases seeds.
      *
      * @return void
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+	    $this->call([
+		    TodoSeeder::class,
+	    ]);
     }
 }
